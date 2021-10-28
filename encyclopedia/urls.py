28c95afbc8e71891ad5cd5b6e5b742224a1e8c6a
Nick-Yk <yakumov80@gmail.com>
2021-10-28 19:00:20 +0300

    add link 8

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name ="wiki"
+
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:title>",views.oarticle, name="oarticle")
+    path("wiki/<str:title>",views.oarticle, name="oarticle")
 ]
