c8819fd349db9b8cb91433d85730b8c08f641d2b
Nick-Yk <yakumov80@gmail.com>
2021-10-28 18:57:28 +0300

    add link 8

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:oarticle>",views.oarticle, name="oarticle")
+    path("<str:title>",views.oarticle, name="oarticle")
 ]
