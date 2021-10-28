a253e21ef0ab55250dd9d6da8cc7042ab66f4974
Nick-Yk <yakumov80@gmail.com>
2021-10-28 16:35:52 +0300

    add link 1

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from . import util
-
+import markdown2
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def oarticle(request, title):
+    content = util.get_entry(title)
+    result = markdown2.Markdown(content)
+    return render(request, "encyclopedia/oartycle.html", {
+        "title": title,
+        "content": result
+    })
