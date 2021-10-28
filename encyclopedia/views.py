827bbff440c36d874a2009836adab8375fa20399
Nick-Yk <yakumov80@gmail.com>
2021-10-28 20:17:51 +0300

    add link 8

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -13,7 +13,7 @@ def index(request):
 def oarticle(request, entry):
     content = util.get_entry(entry)
     print(f"request={content}")
-    print(f"title={title}")
+    print(f"title={entry}")
     result = markdown2.Markdown(content)
     return render(request, "encyclopedia/oarticle.html", {
         "title": entry,
