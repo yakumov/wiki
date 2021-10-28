a9d2ed27bdea45ee5132d308341b2bfbb42e1edb
Nick-Yk <yakumov80@gmail.com>
2021-10-28 19:35:24 +0300

    add link 8

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -8,5 +8,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/css",views.oarticle, name="oarticle")
+    path("css",views.oarticle, name="oarticle")
 ]
