13ba748a768133e84762e735ca21c10efcd6f8de
Nick-Yk <yakumov80@gmail.com>
2021-11-01 12:03:50 +0200

    add search 2

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:entry>",views.oarticle, name="oarticle"),
-    path("search/",views.search, name="search")
+    path("search/",views.search, name="search"),
+    path("add/",views.add, name="add")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django import forms
 
 from . import util
 import markdown2
+
+class NewArticleForms( forms.Form) :
+    article = forms.CharField(label="Найменування статті",widget=forms.TextInput(attrs={'class': 'form-control'}))
+    content = forms.CharField(label= "", widget=forms.Textarea(attrs={'class': 'form-control', 'style' :"height:100%; margin:5px 2px;"}))
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -47,3 +52,26 @@ def search(request):
 #                    "non": str("Відсутні")
 #                    })
 
+def add(request):
+    if request.method == "POST":
+        form = NewArticleForms(request.POST)
+        if form.is_valid() :
+            article = form.cleaned_data["article"]
+            content = form.cleaned_data["content"]
+            old_content=util.get_entry(article)
+            if old_content==None:
+                util.save_entry(article, content)
+                return HttpResponseRedirect(f"/wiki/{oarticle}")
+            else:
+                return render(request, "./encyclopedia/add.html", {
+                        "form": form,
+                        "error": f"Стаття з назвою '{article}' вже існує. Змініть назву."
+                        })
+        else:
+            return render(request, "./encyclopedia/add.html", {
+            "form": form
+            })
+    else:
+        return render(request, "encyclopedia/add.html", {
+            "form": NewArticleForms()
+            })
